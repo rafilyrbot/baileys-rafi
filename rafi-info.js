fac2b591630f2b1203981eaafe5b1f9bdc87e325
rafilyrbot <belajary820@gmail.com>
2025-10-28 19:39:20 +0700

    Add rafi-info.js to display bot information
    This script logs information about the Baileys Public Executor WhatsApp bot, including contact details and build time.

---
+++ rafi-info.js
@@ -0,0 +1,18 @@
+import moment from "moment";
+
+console.log(`
+╔════════════════════════════════════════╗
+║ Baileys Public Executor by rafistore6  ║
+║ WhatsApp Bot Multi-Device (Info)       ║
+╚════════════════════════════════════════╝
+
+📱 Nomer Telpon : 6285883438683
+📱 Telegram     : https://t.me/rafistore6
+💻 GitHub       : https://github.com/rafilybot
+📅 Build at     : ${moment().format("DD MMMM YYYY, HH:mm:ss")}
+
+✅ Baileys Info Mode Aktif
+🥶 Thanks you
+Baileys by Rafi
+──────────────────────────────────────────
+`);
