f43248a2c7a08ebe40b197018dbb2c052fc31bda
rafilyrbot <belajary820@gmail.com>
2025-10-28 14:26:49 +0700

    Add rafi-info.js for Baileys Public Executor info

---
+++ rafi-info.js
@@ -0,0 +1,16 @@
+import moment from "moment"
+import chalk from "chalk"
+
+console.log(chalk.redBright(`╔═════════════════════════════════════╗`))
+console.log(chalk.redBright(`║  Baileys Public Executor by rafistore6`))
+console.log(chalk.redBright(`║  WhatsApp Bot Multi-Device (Info)`))
+console.log(chalk.redBright(`╚═════════════════════════════════════╝\n`))
+
+console.log(chalk.redBright(`📞 Nomer Telpon : 6285883438683`))
+console.log(chalk.redBright(`💬 Telegram     : https://t.me/rafistore6`))
+console.log(chalk.blueBright(`🌐 GitHub       : https://github.com/rafilybot`))
+console.log(chalk.blueBright(`🕒 Build at     : ${moment().format("DD MMMM YYYY, HH:mm:ss")}\n`))
+console.log(chalk.blueBright(`✅ Baileys Info Mode Aktif`))
+console.log(chalk.blueBright(`💬 Thanks you`))
+console.log(chalk.blueBright(`📦 Baileys by Rafi`))
+console.log(chalk.blueBright(`────────────────────────────────────────`))
